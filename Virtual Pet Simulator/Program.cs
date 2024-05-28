16e7c974fb6b1d0c6640582e33cf827b12c03d06
Divya Sirling Edward <Dsirlingedward1882@conestogac.on.ca>
2024-05-27 23:09:27 -0400

    Added detailed project features

--- Virtual Pet Simulator/Program.cs
+++ Virtual Pet Simulator/Program.cs
@@ -8,17 +8,20 @@ namespace VirtualPet
         {
             int hunger = 5;
             int happiness = 5;
-            int health = 5;
+            int health = 8;
             string petType = "";
             string petName = "";
 
+            //welcome message
             Console.WriteLine("Welcome to Virtual Pet Simulator!");
-            Console.WriteLine("Please choose a type of pet:");
+
+            //pet creation
+            Console.WriteLine("Please choose your preferred type of pet");
             Console.WriteLine("1. Cat");
             Console.WriteLine("2. Dog");
             Console.WriteLine("3. Rabbit");
 
-            string userInput = Console.ReadLine();
+            string userInput = Console.ReadLine();  
             switch (userInput)
             {
                 case "1":
@@ -39,26 +42,63 @@ namespace VirtualPet
 
              petName = Console.ReadLine();
 
-            Console.WriteLine("Welcome," + " " + petName + "!");
+            Console.WriteLine("Great!" + " " + "You have created a" + " " + petType + " "+ "named" + " " + petName);
 
             bool exit = false;
 
             while(!exit)
             {
+                 //main menu options
                 Console.WriteLine("\n Main menu:");
                 Console.WriteLine("1. Feed" + " " + petName);
                 Console.WriteLine("2. Play with" + " " + petName);
                 Console.WriteLine("3. Let" + " " + petName + " " + "rest");
                 Console.WriteLine("4. Check" + " " + petName + "'s" + " " + "status");
                 Console.WriteLine("5. Exit");
+
+                 //user choice
                 string choice= Console.ReadLine();
                 switch (choice)
                 {
                     case "1":
-                        Console.WriteLine("You fed" + " " + petName + ".");
+                        hunger -= 2;
+                        //if (hunger < 0)hunger = 0;
+                        health++;
+                        Console.WriteLine("You fed" + " " + petName + "." + " " + "Hunger decreases, and health improves slightly.");
+                        break;
+
+                        case "2":
+                        happiness += 2;
+                        //if(happiness > 10)happiness = 10;
+                        hunger++;
+                        if (hunger > 10) hunger = 10;
+                        Console.WriteLine("You played with" + " " + petName+ "." + "Happiness increases and" + " " + petName + " "+ "is bit hungry.");
+                        break;
+
+                        case "3":
+                        health += 2;
+                        //if (health > 10) health = 10;
+                        happiness--;
+                        if(happiness < 0)happiness=0;
+                        Console.WriteLine("You let" + " " + petName + " " + "rest.");
+                        break;
+
+                        case "4":
+                        Console.WriteLine(petName + "'s" + " " + "status is");
+                        Console.WriteLine("Hunger:" + hunger);
+                        Console.WriteLine("Happiness:" + happiness);
+                        Console.WriteLine("Health:" + health);
+                        break;
+                        case "5":
+                        exit=true;
+                        break;
+                        default:
+                        Console.WriteLine("Invalid choice");
+                        break;
 
                 }
             }
+            Console.WriteLine("Thank You for playing with"+ " " + petName + "" + "!"); 
            
         }
     
